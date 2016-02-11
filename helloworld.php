db89cf6e56566c53cdfdd862da564242c5061b52
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-02-11 12:55:20 +0200

    added if statement

--- helloworld.php
+++ helloworld.php
@@ -10,5 +10,24 @@
 	// for adding up numbers + sign
 	// for adding up strings .
 	echo $first_name." ".$last_name;
-
+	
+	// > greater than
+	// < smaller than
+	// >= greater than or equal
+	// <= smaller than or equal
+	// == equal
+	// != not equal
+	
+	$age = 5;
+	
+	//if age is smaller than or equal to 6, then you can sleep at lunchtime
+	if($age <= 6){
+		//it is true
+		echo " yes you can sleep at lunchtime";
+		
+	}else{
+		//it is false
+		echo " no, you are at school";
+	}
+ 
 ?>
