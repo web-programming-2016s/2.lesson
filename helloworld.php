c96f1dc9017bf7cb2b350039bc69c028dcc75ac2
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-02-11 12:42:43 +0200

    adde first lines of PHP

--- helloworld.php
+++ helloworld.php
@@ -1,1 +1,14 @@
- 
+<?php
+	
+	// Variable starts with $, 
+	//string has to use quotation marks
+	// line ends with semicolon
+	$first_name = "Romil";
+	$last_name = "Rõbtšenkov";
+	
+	// show code to browser
+	// for adding up numbers + sign
+	// for adding up strings .
+	echo $first_name." ".$last_name;
+
+?>
